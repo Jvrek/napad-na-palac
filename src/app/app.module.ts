718728d1977e80d7731675c6ff5a9eaad8c0871a
jaroslaw <spyrkajarek@gmail.com>
2022-05-09 21:15:20 +0200

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { InfoComponent } from './info/info.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { AcceptComponent } from './accept/accept.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    InfoComponent,
+    FooterComponent,
+    ContentComponent,
+    AcceptComponent
   ],
   imports: [
     BrowserModule,
