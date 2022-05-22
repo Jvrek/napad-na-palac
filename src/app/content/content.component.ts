c818f03a67953b364f72a055a3117640c041866a
Jaroslaw Spyrka <jaroslaw.spyrka@gmail.com>
2022-05-23 01:49:35 +0200

    content

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -10,6 +10,30 @@ export class ContentComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+
+    const countDown = new Date(2022,5,4,18,0,0).getTime();
+
+    setInterval(() => {  
+          const now = new Date().getTime();
+          const distance = countDown - now;
+          document.getElementById("days")!.innerText = Math.floor(distance / (day)).toString(),
+          document.getElementById("hours")!.innerText = Math.floor((distance % (day)) / (hour)).toString(),
+          document.getElementById("minutes")!.innerText = Math.floor((distance % (hour)) / (minute)).toString(),
+          document.getElementById("seconds")!.innerText = Math.floor((distance % (minute)) / second).toString();
+
+        //do something later when date is reached
+        if (distance < 0) {
+          document.getElementById("headline")!.innerText = "to już!";
+          document.getElementById("countdown")!.style.display = "none";
+          document.getElementById("content")!.style.display = "block";
+        }
+        //seconds
+      }, 1)
   }
 
 }
